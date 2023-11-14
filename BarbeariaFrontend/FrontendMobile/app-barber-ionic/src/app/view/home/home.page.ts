48b497ad25a5a5d9c5f2c6bd68371afd8071e760
tanakadanilo <>
2023-11-13 21:59:32 -0300

    Implementado as push notifications

--- BarbeariaFrontend/FrontendMobile/app-barber-ionic/src/app/view/home/home.page.ts
+++ BarbeariaFrontend/FrontendMobile/app-barber-ionic/src/app/view/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PushNotifications } from '@capacitor/push-notifications';
 import { MenuController } from '@ionic/angular';
 
 @Component({
@@ -12,35 +13,59 @@ export class HomePage {
 
   responsiveOptions: any[] | undefined;
 
-  constructor(private menuCtrl: MenuController) {}
+  constructor(private menuCtrl: MenuController) {
+  }
 
   ngOnInit() {
 
-    this.horarios = ["08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30",];
+    this.addListeners();
 
-    // this.responsiveOptions = [
-    //   {
-    //       breakpoint: '1199px',
-    //       numVisible: 1,
-    //       numScroll: 1
-    //   },
-    //   {
-    //       breakpoint: '991px',
-    //       numVisible: 2,
-    //       numScroll: 1
-    //   },
-    //   {
-    //       breakpoint: '767px',
-    //       numVisible: 1,
-    //       numScroll: 1
-    //   }
-    // ];
+    this.registerNotifications();
+
+    this.getDeliveredNotifications();
 
   }
 
   openEndMenu() {
     // Open the menu by side
     this.menuCtrl.open('end');
+  }
+
+  addListeners = async () => {
+    await PushNotifications.addListener('registration', token => {
+      console.info('Registration token: ', token.value);
+    });
+  
+    await PushNotifications.addListener('registrationError', err => {
+      console.error('Registration error: ', err.error);
+    });
+  
+    await PushNotifications.addListener('pushNotificationReceived', notification => {
+      console.log('Push notification received: ', notification);
+    });
+  
+    await PushNotifications.addListener('pushNotificationActionPerformed', notification => {
+      console.log('Push notification action performed', notification.actionId, notification.inputValue);
+    });
+  }
+  
+  registerNotifications = async () => {
+    let permStatus = await PushNotifications.checkPermissions();
+  
+    if (permStatus.receive === 'prompt') {
+      permStatus = await PushNotifications.requestPermissions();
+    }
+  
+    if (permStatus.receive !== 'granted') {
+      throw new Error('User denied permissions!');
+    }
+  
+    await PushNotifications.register();
+  }
+  
+  getDeliveredNotifications = async () => {
+    const notificationList = await PushNotifications.getDeliveredNotifications();
+    console.log('delivered notifications', notificationList);
   }
 
 }
