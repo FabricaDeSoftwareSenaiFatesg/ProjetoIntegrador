15f2f68b43ca098a15ba4da05bd7e9bc1db9bfbc
Magno Souza <magno.souza@docnix.com.br>
2023-11-28 19:52:11 -0300

    corrigindo comportamento do interceptor

--- BarbeariaFrontend/FrontendMobile/app-barber-ionic/src/arquitetura/services/http-interceptor.ts
+++ BarbeariaFrontend/FrontendMobile/app-barber-ionic/src/arquitetura/services/http-interceptor.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {NavController} from "@ionic/angular";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpInterceptorAuth {
+export class HttpInterceptorAuth implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private navigation: NavController) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let token = localStorage.getItem("ads_access_token");
+
+    if (token != null) {
+      request = request.clone({
+        setHeaders: {
+          'Content-Type': 'application/json',
+          Authorization: `bearer ${token}`
+        }
+      });
+    }
+
+    return next.handle(request).pipe(tap(() => {
+    }, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          localStorage.removeItem('ads_access_token');
+          this.navigation.navigateRoot(['login']);
+        }
+      }
+    }));
+  }
+
+
+
 }
