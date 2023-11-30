b47496cdb246b3600cdcfdc6a20f63e5a38a94fc
Magno Souza <magno.souza@docnix.com.br>
2023-11-29 22:05:03 -0300

    adicionando validações do login

--- BarbeariaFrontend/FrontendMobile/app-barber-ionic/src/app/view/login/login.module.ts
+++ BarbeariaFrontend/FrontendMobile/app-barber-ionic/src/app/view/login/login.module.ts
@@ -6,15 +6,17 @@ import { LoginPage } from './login.page';
 import { LoginPageRoutingModule } from './login-routing.module';
 import { CarouselModule } from 'primeng/carousel';
 import {FormsModule} from "@angular/forms";
+import {MessagesModule} from "primeng/messages";
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    LoginPageRoutingModule,
-    CarouselModule,
-    FormsModule
-  ],
+    imports: [
+        IonicModule,
+        CommonModule,
+        LoginPageRoutingModule,
+        CarouselModule,
+        FormsModule,
+        MessagesModule
+    ],
   declarations: [LoginPage]
 })
 export class LoginPageModule {}
